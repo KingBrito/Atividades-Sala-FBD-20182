3e3942c4c5327309fcd4f13ea50eee57fedb8835
victor-pc <victor.aefarias@gmail.com>
2018-09-26 11:23:22 -0300

    Today's class

--- 00_Listas/lista5.sql
+++ 00_Listas/lista5.sql
@@ -12,7 +12,7 @@ WHERE NOT EXISTS (
 )
 
 -- b)
-- Aqui não rola!
+-- Aqui não rola!
 SELECT p.pid, p.pnome
 FROM pecas p, fornpecaproj fpj, fornecedor f
 WHERE p.pid=fpj.pid AND fpj.fid=f.fid AND f.fnome<>'Maria Silva' AND fnome<>'João Silva'
@@ -25,3 +25,23 @@ WHERE NOT EXISTS(
 	WHERE p.pid=fpj.pid AND fpj.fid=f.fid AND (f.fnome='Maria Silva' OR f.fnome='João Silva') 
 )
 
+
+-- g)
+SELECT fpj.pid
+FROM fornpecaproj fpj
+GROUP BY fpj.pid
+HAVING count(DISTINCT fpj.fid)>1
+
+-- c)
+-- nao aparece quem ta com 0
+SELECT fpj.pid, sum(fpj.qtd)
+FROM fornpecaproj fpj
+GROUP BY fpj.pid
+
+SELECT p.pid, (select sum(fpj.qtd) from fornpecaproj fpj where fpj.pid=p.pid)
+FROM pecas p
+
+-- h)
+SELECT f.fid, sum(fpj.qtd)
+FROM fornecedor f LEFT OUTER JOIN fornpecaproj fpj ON f.fid=fpj.fid
+GROUP BY f.fid
